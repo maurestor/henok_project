f7ab9e247337cd6d0f2f82a3ce1ee389dfb84a8c
maurestor <restor@gmail.com>
2023-05-13 22:10:58 -0500

    Creadon inicial

---
+++ sms.py
@@ -0,0 +1,21 @@
+import time
+import serial
+
+recipient = "+1234567890"
+message = "Hello, World!"
+
+phone = serial.Serial("/dev/ttyACM0",  460800, timeout=5)
+try:
+    time.sleep(0.5)
+    phone.write(b'ATZ\r')
+    time.sleep(0.5)
+    phone.write(b'AT+CMGF=1\r')
+    time.sleep(0.5)
+    phone.write(b'AT+CMGS="' + recipient.encode() + b'"\r')
+    time.sleep(0.5)
+    phone.write(message.encode() + b"\r")
+    time.sleep(0.5)
+    phone.write(bytes([26]))
+    time.sleep(0.5)
+finally:
+    phone.close()
